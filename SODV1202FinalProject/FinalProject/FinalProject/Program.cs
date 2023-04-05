2985bbb1f64623bbf9c0a099795ebc64b94aa351
Xaynerd <125408435+Xaynerd@users.noreply.github.com>
2023-04-05 15:41:29 -0700

    Finished
    To be completely honest, I thought this would take longer than it did. And I mean yeah, it's not perfect or anything, but I'm pretty sure it's more than good enough. Time to work on my other finals!

--- SODV1202FinalProject/FinalProject/FinalProject/Program.cs
+++ SODV1202FinalProject/FinalProject/FinalProject/Program.cs
@@ -52,13 +52,13 @@ namespace MyProject
         }
         public void Move(int playerMove, Player player)
         {
-            bool found = false;
-            int filled = 0;
-            string playerPiece = player.GetPlayerPiece(); //retrieves the player's piece
+            bool found = false;                             //used to break out of loop when it finds the lowest most spot
+            int filled = 0;                                 //used to check how full a column is
+            string playerPiece = player.GetPlayerPiece();   //retrieves the player's piece
             for (int i = 5; i >= 0; i--)
             {
                 if (found) {
-                    break;
+                    break; //breaks out of loop
                 }
                 else
                 {
@@ -71,7 +71,7 @@ namespace MyProject
                         if (filled == 6)
                         {
                             Console.WriteLine("Sorry, this column is full already buddy. Here, I'll put it somewhere randomly for you!"); //i want to move this into main somehow so then it persists through console.clear, though if not ill just get rid of console.clear ;)
-                            Random ran = new Random();
+                            Random ran = new Random(); //thought it would be fun to add a random element :)
                             bool find = false;
                             while (find == false)
                             {
@@ -90,6 +90,75 @@ namespace MyProject
 
             }
         }
+        public bool CheckWin(Player player)
+        {
+            //i was dreading this part
+            string playerPiece = player.GetPlayerPiece();
+
+            //check rows
+            for (int row = 0; row < 6; row++)//will not search _ row
+            {
+                for (int col = 1; col < 5; col++) //wont bother searching the | columns as that would be a waste of computing power, even if it doesnt matter too much at this level
+                {
+                    if (BoardArray[row, col] == playerPiece &&
+                        BoardArray[row, col + 1] == playerPiece &&
+                        BoardArray[row, col + 2] == playerPiece &&
+                        BoardArray[row, col + 3] == playerPiece)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //check columns
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 1; col < 8; col++)
+                {
+                    if (BoardArray[row, col] == playerPiece &&
+                        BoardArray[row + 1, col] == playerPiece &&
+                        BoardArray[row + 2, col] == playerPiece &&
+                        BoardArray[row + 3, col] == playerPiece)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //check diagonal top left to bottom right
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 1; col < 5; col++)
+                {
+                    if (BoardArray[row, col] == playerPiece &&
+                        BoardArray[row + 1, col + 1] == playerPiece &&
+                        BoardArray[row + 2, col + 2] == playerPiece &&
+                        BoardArray[row + 3, col + 3] == playerPiece)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //check diagonal top right to bottom left
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 3; col < 8; col++)
+                {
+                    if (BoardArray[row, col] == playerPiece &&
+                        BoardArray[row + 1, col - 1] == playerPiece &&
+                        BoardArray[row + 2, col - 2] == playerPiece &&
+                        BoardArray[row + 3, col - 3] == playerPiece)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //no win found
+            return false;
+        }
+
 
     }
 
@@ -97,13 +166,13 @@ namespace MyProject
     public class Controls : Board
     {
         public int GetMove()
-        {
+        {   //loop until a number between 1-7 is given
             while (true) //https://stackoverflow.com/questions/28955029/how-do-i-convert-a-console-readkey-to-an-int-c-sharp
-            {
+            {            //took a minute looking around for this one
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                 int number;
-                bool isNumber = int.TryParse(keyInfo.KeyChar.ToString(), out number);
-                if (isNumber && number > 0 && number < 8) //i had it so it would check later, but it makes more sense just to do it here and loop until it gets a proper result
+                bool isNumber = int.TryParse(keyInfo.KeyChar.ToString(), out number); //was having a problem with it crashing if you input something that wasnt a number
+                if (isNumber && number > 0 && number < 8) 
                 {
                     return number;
                 } 
@@ -120,12 +189,15 @@ namespace MyProject
     {
         public bool Turn { get; set; }
         public Player() //i havent fully wrapped my head aroudn classes and stuff so somethings are gonna be kinda off. like this, but it works so who cares
-        {
+        {               
             Turn = true;
         }
         public abstract string GetPlayerPiece();
-        public abstract bool PlayerTurn();
-        public void EndTurn()
+        public bool PlayerTurn()
+        {
+            return Turn;
+        }
+        public void EndTurn()//could maybe get away with combining these somehow, but i just cant think of it so them being split up works just fine
         {
             Turn = false;
         }
@@ -140,10 +212,6 @@ namespace MyProject
         {
             return "x";
         }
-        public override bool PlayerTurn()
-        {
-            return Turn;
-        }
     }
 
     public class Player2 : Player
@@ -151,10 +219,6 @@ namespace MyProject
         public override string GetPlayerPiece()
         {
             return "o";
-        }
-        public override bool PlayerTurn()
-        {
-            return Turn;
         }
     }
 
@@ -167,36 +231,53 @@ namespace MyProject
             Player player1 = new Player1();
             Player player2 = new Player2();
             bool gameRunning = true;
-            Console.WriteLine(player1.PlayerTurn());
-            Console.WriteLine(player2.PlayerTurn());
+            int winner = 0;
 
             game.CreateBoard();
             game.Display();
-            //game.BoardArray[5, 1] = "x";
-            //game.Display();
-            //game.Move(control.GetMove(), player1);
-            //game.Display();
 
             while (gameRunning)
             {
-                Console.Clear();
-                game.Display();
+                //Console.Clear();  there is an error message in one of the class methods that i want seen, so this wont do, unless i add a delay somewhere it will never be seen :(      
                 if (player1.PlayerTurn() == true)
                 {
                     Console.WriteLine("PLAYER 1 TURN");
                     game.Move(control.GetMove(), player1);
                     player1.EndTurn();
-                    player2.StartTurn();
-                    //Console.WriteLine(player1.PlayerTurn());
-                    //Console.WriteLine(player2.PlayerTurn());
-                } else if (player2.PlayerTurn() == true)
+
+                    if (game.CheckWin(player1) == true)
+                    {
+                        winner = 1;
+                        gameRunning = false;
+                    } 
+                    else
+                    {
+                        player2.StartTurn(); //prevent player2's turn from starting after player 1 wins, though i forget if it actually does start player2's turn :/
+                    }
+
+                } 
+                else if (player2.PlayerTurn() == true)
                 {
                     Console.WriteLine("PLAYER 2 TURN");
                     game.Move(control.GetMove(), player2);
                     player2.EndTurn();
-                    player1.StartTurn();
-                }               
+                    
+                    if (game.CheckWin(player2) == true)
+                    {
+                        winner = 2;
+                        gameRunning = false;
+                    }
+                    else
+                    {
+                        player1.StartTurn(); //prevent player1 turn from starting after victory
+                    }
+                }
+                game.Display();
             }
+
+            Console.WriteLine();
+            Console.WriteLine(winner == 1 ? "Congrats on your victory over player 2! You win player 1!\n" : "Congrats on your victory over player 1! You win player 2!\n");
+            Console.WriteLine("Until next time o/");
             
         }
     }
