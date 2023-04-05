4f41ffdd5cfb18e7a12f4d6e9a1db59b79c0c2bb
Xaynerd <125408435+Xaynerd@users.noreply.github.com>
2023-04-04 18:57:11 -0700

    Add files via upload
    only got about one more session needed for this, just need to get the actual like "connect 4" recognition. no idea how im gonna do that, ill probably just sleep on it. maybe ill think of something at 3 am.

--- SODV1202FinalProject/FinalProject/FinalProject/Program.cs
+++ SODV1202FinalProject/FinalProject/FinalProject/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MyProject
 {
@@ -48,46 +50,111 @@ namespace MyProject
 
             }
         }
-
-        public virtual bool PlayerTurn()
+        public void Move(int playerMove, Player player)
         {
-            return false;
+            bool found = false;
+            int filled = 0;
+            string playerPiece = player.GetPlayerPiece(); //retrieves the player's piece
+            for (int i = 5; i >= 0; i--)
+            {
+                if (found) {
+                    break;
+                }
+                else
+                {
+                    if (BoardArray[i,playerMove] == " ") {
+                        BoardArray[i, playerMove] = playerPiece; //replaces the bottom-most empty position with the player piece
+                        found = true;
+                    } else
+                    {
+                        filled++;
+                        if (filled == 6)
+                        {
+                            Console.WriteLine("Sorry, this column is full already buddy. Here, I'll put it somewhere randomly for you!"); //i want to move this into main somehow so then it persists through console.clear, though if not ill just get rid of console.clear ;)
+                            Random ran = new Random();
+                            bool find = false;
+                            while (find == false)
+                            {
+                                int rand = ran.Next(1, 8);
+                                if (rand != playerMove) //you know it would be a lot simpler to just get them to chose again, but guh, i refuse!
+                                {
+                                    find = true;
+                                    Move(rand, player); //makes a mystery move for the player :)
+                                }
+                            }
+                            
+                            
+                        }
+                    }
+                }
+
+            }
         }
 
     }
-    public abstract class Player1 : Board
-    {
-        public bool Turn;
-        public virtual bool PlayerTurn()
-        {
-            // implementation for Player1's turn
-            Turn = true;
-        }
-    }
 
-    public abstract class Player2 : Board
-    {
-        public bool Turn;
-        public virtual bool PlayerTurn()
-        {
-            // implementation for Player2's turn
-            Turn = true;
-        }
-    }
 
     public class Controls : Board
     {
+        public int GetMove()
+        {
+            while (true) //https://stackoverflow.com/questions/28955029/how-do-i-convert-a-console-readkey-to-an-int-c-sharp
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                int number;
+                bool isNumber = int.TryParse(keyInfo.KeyChar.ToString(), out number);
+                if (isNumber && number > 0 && number < 8) //i had it so it would check later, but it makes more sense just to do it here and loop until it gets a proper result
+                {
+                    return number;
+                } 
+                else
+                {
+                    Console.WriteLine("That is not a valid input, please enter a number between 1-7");
+                }
+            }
+        }
+
+    }
+
+    public abstract class Player
+    {
+        public bool Turn { get; set; }
+        public Player() //i havent fully wrapped my head aroudn classes and stuff so somethings are gonna be kinda off. like this, but it works so who cares
+        {
+            Turn = true;
+        }
+        public abstract string GetPlayerPiece();
+        public abstract bool PlayerTurn();
+        public void EndTurn()
+        {
+            Turn = false;
+        }
+        public void StartTurn()
+        {
+            Turn = true;
+        }
+    }
+    public class Player1 : Player
+    {
+        public override string GetPlayerPiece()
+        {
+            return "x";
+        }
         public override bool PlayerTurn()
         {
-            // call the appropriate player's turn method based on whose turn it is
-            if (Player1.Turn)
-            {
-                Player1.PlayerTurn();
-            }
-            else if (Player2.Turn)
-            {
-                Player2.PlayerTurn();
-            }
+            return Turn;
+        }
+    }
+
+    public class Player2 : Player
+    {
+        public override string GetPlayerPiece()
+        {
+            return "o";
+        }
+        public override bool PlayerTurn()
+        {
+            return Turn;
         }
     }
 
@@ -96,11 +163,40 @@ namespace MyProject
         static void Main(string[] args)
         {
             Board game = new Board();
+            Controls control = new Controls();
+            Player player1 = new Player1();
+            Player player2 = new Player2();
+            bool gameRunning = true;
+            Console.WriteLine(player1.PlayerTurn());
+            Console.WriteLine(player2.PlayerTurn());
+
             game.CreateBoard();
             game.Display();
-            game.BoardArray[4, 4] = "x";
-            game.Display();
+            //game.BoardArray[5, 1] = "x";
+            //game.Display();
+            //game.Move(control.GetMove(), player1);
+            //game.Display();
 
+            while (gameRunning)
+            {
+                Console.Clear();
+                game.Display();
+                if (player1.PlayerTurn() == true)
+                {
+                    Console.WriteLine("PLAYER 1 TURN");
+                    game.Move(control.GetMove(), player1);
+                    player1.EndTurn();
+                    player2.StartTurn();
+                    //Console.WriteLine(player1.PlayerTurn());
+                    //Console.WriteLine(player2.PlayerTurn());
+                } else if (player2.PlayerTurn() == true)
+                {
+                    Console.WriteLine("PLAYER 2 TURN");
+                    game.Move(control.GetMove(), player2);
+                    player2.EndTurn();
+                    player1.StartTurn();
+                }               
+            }
             
         }
     }
